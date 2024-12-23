406ed888aca3923c604bce495451e91916bf9724
Sevastyanov Ilya <sevastyanov-1990@bk.ru>
2024-12-24 00:34:15 +0300

    изменение недоступности поля email1

--- myproject.py
+++ myproject.py
@@ -1,5 +1,5 @@
 from app import app
-
-# Этот файл содержит запуск приложения
-if __name__ == '__main__':
-     app.run(debug=True)
+#
+# # Этот файл содержит запуск приложения
+# if __name__ == '__main__':
+#      app.run(debug=True)
